731d0c8af71b1370f580a68accf9f7fcc36dacb3
Brian Falasz <bfalasz@yahoo.com>
2019-08-12 21:13:16 -0500

    learned a lot math expressions in python. Also converted ints to floats

---
+++ ex3.py
@@ -0,0 +1,34 @@
+# printing a string to the terminal
+print("I will now count my chickens:")
+
+# calculating the numbers after the comma using addition and division
+print("Hens", 25.0 + 30.0 / 6.0)
+
+# Printing a string to the terminal
+print("Now I will count the eggs:")
+
+# Doing a bunch of calculations: adding the first three numbers subtracting the fourth, checking the divisibilty of the fifth
+# subtracting the sixth, dividing that by the seventh and finally adding that to the eighth
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Printing out a string to the terminal
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# adding the first to numbers and then checking if it's less than the result of the third and fourth which is subtracted
+# Prints the boolean False 
+print(3.0 + 2.0 < 5.0 - 7.0)
+ 
+# Prints a string and after the comma returns the result of 3 + 2 
+print("What is 3 + 2?", 3.0 + 2.0)
+# Same thing as line above except it is subtracting the numbers after the comma
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# Printing out a string to the terminal
+print("Oh, that's why it's False.")
+# Printing out a string to the terminal
+print("How about some more.")
+
+# These next three print statements do basically the same thing. Returns the string and then the boolean value of the result.
+print("Is it greater", 5.0 > -2.0)
+print("Is it greater or equal", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)

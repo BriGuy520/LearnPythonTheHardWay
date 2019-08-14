fe48f317631bbb27700e959af008c84890df11d0
Brian Falasz <bfalasz@yahoo.com>
2019-08-13 20:37:07 -0500

    did some more printing exercises and learned about a new paramaeter called end which is part of the print function

---
+++ ex7.py
@@ -0,0 +1,27 @@
+print("Mary had a little lamb.")
+# using the format() method to add the word snow inside of the curly({}) brackets
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+# Using multiplication(*) to print out 10 periods
+print("." * 10) # What'd that do? Print 10 periods
+
+# creating a seperate variable for each character in the words cheese burger
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end. Try removing it to see what happens 
+# end is a special parameter for the print function and will keep the function and the next print function on the 
+# same line and concatinate it with whatever we put in the string
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' + ')
+print(end7 + end8 + end9 + end10 + end11 + end12, end=' = Delicious!')
+

8545f9e344089910d8dff5e8d68ad68b4f56ec40
Brian Falasz <bfalasz@yahoo.com>
2019-08-14 20:28:53 -0500

    learned more about tuple unpacking and using the input function

--- ex13.py
+++ ex13.py
@@ -2,7 +2,18 @@ from sys import argv
 # read the WYSS section for how to run this
 script, first, second, third = argv
 
+# print("The script is called ", script)
+# print("Your first variable is: ", first)
+# print("Your second variable is: ", second)
+# print("Your third variable is: ", third)
+
 print("The script is called ", script)
 print("Your first variable is: ", first)
 print("Your second variable is: ", second)
 print("Your third variable is: ", third)
+
+color = input("What is your favorite color? ")
+car = input("What is your favorite car? ")
+
+print(f"You should buy a {color} {car}")
+

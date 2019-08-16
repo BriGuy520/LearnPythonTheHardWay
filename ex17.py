793987a4a8093793c999502b17ab1d7dfa8e0faf
Brian Falasz <bfalasz@yahoo.com>
2019-08-15 20:03:57 -0500

    refactored some of my code in ex17 to make it shorter

--- ex17.py
+++ ex17.py
@@ -6,18 +6,20 @@ script, from_file, to_file = argv
 print(f"Copying from {from_file} to {to_file}")
 
 # we could do these on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+in_file = open(from_file).read()
 
-print(f"The input file is {len(indata)} bytes long")
+
+print(f"The input file is {len(in_file)} bytes long")
 
 print(f"Does the output file exist? {exists(to_file)}")
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+# out_file = open(to_file, 'w').write(in_file)
+with open(to_file, 'w') as out_file: 
+  out_file.write(in_file)
 
 print("Alright, all done.")
 
-out_file.close()
-in_file.close()
+# out_file.close()
+# in_file.close()

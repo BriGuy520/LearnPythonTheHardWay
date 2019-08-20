9ce704654784fd42421f60b9b5d8da6dadca1232
Brian Falasz <brianfalasz@gmail.com>
2019-08-19 22:05:46 -0500

    wrote a script that alters a list in python

--- ex35.py
+++ ex35.py
@@ -4,6 +4,7 @@ def gold_room():
   print("This room is full of gold. How much do you take?")
 
   choice = input("> ")
+
   if "0" in choice or "1" in choice:
     how_much = int(choice)
   else: 
@@ -18,7 +19,7 @@ def bear_room():
   print("There is a bear here.")
   print("The bear has a bunch of honey.")
   print("The fat bear is in front of another door.")
-  print("How are you going to moce the bear?")
+  print("How are you going to move the bear?")
   bear_moved = False
 
   while True:

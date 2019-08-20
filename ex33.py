8cb5052c3e84040aa50b56b8b960d4ec08fbcc2d
Brian Falasz <brianfalasz@gmail.com>
2019-08-19 20:37:44 -0500

    made some modifications to the while loop by putting it in a function and creating a seperate function that takes two arguments

--- ex33.py
+++ ex33.py
@@ -1,15 +1,66 @@
-i = 0
-numbers = []
+# This is how we set global variables
 
-while i < 6:
-  print(f"At the top is {i}")
-  numbers.append(i)
+# i = 0
+# numbers = []
 
-  i += 1
-  print("Numbers now: " , numbers)
-  print(f"At the bottom i is {i}")
+# def while_loop(int):
+    # Use global before varible to set it globally so we can use it inside of the function
+#   global i
 
-print("The numbers: ")
+#   while i <= int:
 
-for num in numbers:
-  print(num)
+#     print(f"At the top is {i}")
+#     numbers.append(i)
+
+#     i += 1
+#     print("Numbers now: " , numbers)
+#     print(f"At the bottom i is {i}")
+
+#   print("The numbers: ")
+
+#   for num in numbers:
+#     print(num)
+  
+# while_loop(11)
+
+# def while_loop(int):
+  
+#   i = 0
+#   numbers = []
+
+#   while i <= int:
+
+#     print(f"At the top is {i}")
+#     numbers.append(i)
+
+#     i += 1
+#     print("Numbers now: " , numbers)
+#     print(f"At the bottom i is {i}")
+
+#   print("The numbers: ")
+
+#   for num in numbers:
+#     print(num)
+  
+# while_loop(11)
+
+
+def while_loop(first_int, last_int):
+
+  numbers = []
+  
+  while first_int <= last_int:
+
+    print(f"At the top is {first_int}")
+    numbers.append(first_int)
+
+    first_int += 1
+    print("Numbers now: " , numbers)
+    print(f"At the bottom i is {first_int}")
+
+  print("The numbers: ")
+
+  for num in numbers:
+    print(num)
+  
+while_loop(5, 11)

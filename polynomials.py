cd90e4d5de94edfa8bfe96cfb9d5047026440997
Brian Falasz <brianfalasz@gmail.com>
2019-11-05 20:33:45 -0600

    refactored the existing code

--- polynomials.py
+++ polynomials.py
@@ -5,13 +5,11 @@ class Polynomial:
     self.coeffs = coeffs
 
   def __repr__(self):
-    return "Polynomial(*{!r})".format(self.coeffs)
-  
+    return 'Polynomial(*{!r})'.format(self.coeffs)
+
   def __add__(self, other):
     return Polynomial(*(x + y for x, y in zip(self.coeffs, other.coeffs)))
-
 
 p1 = Polynomial(1,2,3) # x^2 + 2x + 3
 p2 = Polynomial(3,4,3) # 3x^2 + 4x + 3
 
-print(p1 + p2)
